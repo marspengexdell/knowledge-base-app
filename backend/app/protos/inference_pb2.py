e1d960604d9e9a08537e409fb226e6667e3a351f
marspengexdell <marspeng39@gmail.com>
2025-06-22 23:25:47 +0800

    Add python_package option to proto and adjust imports

--- backend/app/protos/inference_pb2.py
+++ backend/app/protos/inference_pb2.py
@@ -28,7 +28,7 @@ DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0finference.prot
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
-_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'inference_pb2', _globals)
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'app.protos.inference_pb2', _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
   DESCRIPTOR._loaded_options = None
   _globals['_MODELTYPE']._serialized_start=474
