658dd916bbaf741a5a233f89c3ad4b0a820d03e4
marspengexdell <marspeng39@gmail.com>
2025-06-30 15:55:10 +0800

    1

--- frontend-user/postcss.config.js
+++ frontend-user/postcss.config.js
@@ -1,6 +1,7 @@
+// frontend-user/postcss.config.js (正确配置)
 export default {
   plugins: {
-    '@tailwindcss/postcss': {},
+    '@tailwindcss/postcss': {}, // 使用新的、正确的插件名
     autoprefixer: {},
   },
 }
