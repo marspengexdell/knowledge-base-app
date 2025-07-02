00fa831d306e3c88facb11f3d57440fea39fb8d1
marspengexdell <marspeng39@gmail.com>
2025-07-02 14:26:04 +0800

    d

--- modules/backend/app/protos/__init__.py
+++
@@ -1,8 +0,0 @@
-# Helper utilities for importing generated gRPC modules.
-
-import os
-import sys
-
-_PROTO_DIR = os.path.dirname(__file__)
-if _PROTO_DIR not in sys.path:
-    sys.path.insert(0, _PROTO_DIR)
